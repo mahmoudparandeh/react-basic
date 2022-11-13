a9a0cda671feb927098ef9799df3c8058d90993d
Mahmoud Parandeh <parandeh@tabrizu.ac.ir>
2022-11-13 16:38:00 +0330

    add db.json

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './components/header-class';
+import JSON from './database/db.json';
 
-const App = () => (
-    <React.Fragment>
-        <Header/>
-    </React.Fragment>
-);
+const App = () => {
+    console.log(JSON);
+    return (
+        <React.Fragment>
+            <Header/>
+        </React.Fragment>
+    );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App/>)
